6f6ee0ebc76447099131179538adbf8bff857e15
Bethany Baker <bakerbethanymarie@gmail.com>
2019-03-24 09:30:30 -0700

    iterate

--- sudo_beauty.py
+++ sudo_beauty.py
@@ -1,12 +1,14 @@
-script = 'this is my script'
-
-print(f"{script}")
-
 import os
-print(os.environ['HOME'])
 
-print(os.environ)
+# script = 'this is my script'
+# print(f"{script}")
 
+# print(os.environ['HOME'])
+# print(os.environ)
+
+for a in os.environ:
+    print('Var: ', a, 'Value: ', os.getenv(a))
+print("all done")
 
 
 
