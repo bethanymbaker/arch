d009da6caafcd03c68124c19cfccc3002121715a
Bethany Baker <bakerbethanymarie@gmail.com>
2019-03-24 09:34:02 -0700

    more better

--- sudo_beauty.py
+++ sudo_beauty.py
@@ -1,16 +1,9 @@
 import os
 
-# script = 'this is my script'
-# print(f"{script}")
-
 # print(os.environ['HOME'])
 # print(os.environ)
 
 for a in os.environ:
     print(f"Var: {a}; Value: {os.getenv(a)}")
-    # print('Var: ', a, 'Value: ', os.getenv(a))
-print("all done")
-
-
 
 print("Hi I love you Alex")
