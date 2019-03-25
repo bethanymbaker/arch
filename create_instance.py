2040c68eed0bfcf4ca39e3cf449b7686e14c3ad0
Bethany Baker <bakerbethanymarie@gmail.com>
2019-03-25 02:56:51 -0700

    cleanup

--- create_instance.py
+++ create_instance.py
@@ -1,24 +1,4 @@
-# gcp_bash_command = """
-# gcloud beta compute --project=project_arch \
-# instances create instance_0 \
-# --zone=us-west1-b \
-# --machine-type=n1-standard-8 \
-# --subnet=default \
-# --maintenance-policy=TERMINATE \
-# --accelerator=type=nvidia-tesla-k80,count=1 \
-# --tags=http-server,https-server \
-# --image=ubuntu-1604-xenial-v20180522 \
-# --image-project=ubuntu-os-cloud \
-# --boot-disk-size=50GB \
-# --boot-disk-type=pd-ssd
-# """
 
 ssh_into_instance = """
 gcloud compute --project "iris-226418" ssh --zone "us-east1-b" "alex"
 """
-
-
-# bashCommand = "cwm --rdf test.rdf --ntriples > test.nt"
-# import subprocess
-# process = subprocess.Popen(ssh_into_instance.split(), stdout=subprocess.PIPE)
-# output, error = process.communicate()
