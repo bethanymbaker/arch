81b3500f69917464a3cc97769f6ed90505fb783f
Teespring11318 <bethany.baker@teespring.com>
2019-03-27 11:34:32 -0700

    fixed

--- vm_instance.py
+++ vm_instance.py
@@ -1,4 +1,13 @@
+import subprocess
 
-ssh_into_instance = """
-gcloud compute --project "iris-226418" ssh --zone "us-east1-b" "alex"
+command_1 = 'gcloud config set project iris-226418'
+
+process = subprocess.Popen(command_1.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+
+ssh_command = """
+gcloud compute --project "iris" ssh --zone "us-east1-b" "arceus"
 """
+
+process = subprocess.Popen(ssh_command.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
