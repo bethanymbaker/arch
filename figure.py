04d4bedd3599bd904408685d3604980e92f3080b
Bethany Baker <bakerbethanymarie@gmail.com>
2019-04-17 02:17:45 -0700

    get dummies

--- figure.py
+++ figure.py
@@ -18,7 +18,12 @@ origination_names = ["fico", "first_payment_date", "is_first_time_home_buyer", "
                      "is_ppmt_pnlty", "prod_type", "state", "prop_type", "zipcode", "id_loan", "loan_purpose",
                      "orig_loan_term", "num_borr", "seller_name", "servicer_name", "is_super_conforming"]
 
-origination = pd.read_csv(origination_data_file, header=None, delimiter='|', names=origination_names,
+origination = pd.read_csv(origination_data_file,
+                          header=None,
+                          delimiter='|',
+                          names=origination_names,
+                          parse_dates=['first_payment_date', 'maturity_date'],
+                          date_parser=lambda x: pd.datetime.strptime(x, '%Y%m'),
                           na_values={'fico': 9999,
                                      'is_first_time_home_buyer': 9,
                                      'mort_ins_pct': 999,
@@ -30,38 +35,38 @@ origination = pd.read_csv(origination_data_file, header=None, delimiter='|', nam
                                      'channel': 9,
                                      'prop_type': 99,
                                      'loan_purpose': 9,
-                                     'num_borr': 99})
-# dtype={'msa': 'category',
-#        'occpy_sts': 'category',
-#        'channel': 'category',
-#        'state': 'category',
-#        'prop_type': 'category',
-#        'zipcode': 'category',
-#        'loan_purpose': 'category',
-#        'num_borr': 'category'})
+                                     'num_borr': 99})\
+    .drop(columns='prod_type')
 
-del origination['prod_type']
+category_columns = ['is_first_time_home_buyer',
+                    'channel',
+                    'is_ppmt_pnlty',
+                    'prop_type',
+                    'msa',
+                    'occpy_sts',
+                    'state',
+                    'zipcode',
+                    'loan_purpose',
+                    'num_borr',
+                    'is_super_conforming']
 
-origination[['first_payment_date', 'maturity_date']] = origination[['first_payment_date', 'maturity_date']]\
-    .apply(pd.to_datetime, format='%Y%m')
-origination.is_first_time_home_buyer = origination.is_first_time_home_buyer.map({'Y': True, 'N': False}).astype(bool)
-origination.is_ppmt_pnlty = origination.is_ppmt_pnlty.map({'Y': True, 'N': False}).astype(bool)
-origination.is_super_conforming = pd.isna(origination.is_super_conforming)
+for col in category_columns:
+    new_col = origination[col].astype('category')
+    dumm = pd.get_dummies(new_col, prefix=col, drop_first=True, dummy_na=True)
+    for coll in dumm.columns:
+        origination[coll] = dumm[coll]
+    del origination[col]
 
-origination['msa_code'] = origination.msa.cat.codes
-
-dum_1 = pd.get_dummies(origination.msa, prefix='msa', drop_first=True)
-
-
-
-# origination.set_index('id_loan', inplace=True)
+origination.set_index(['id_loan', 'first_payment_date', 'maturity_date'], inplace=True)
+del origination['seller_name']
+del origination['servicer_name']
 
 # Look at distributions
 # sns.distplot(origination.fico[~origination.fico.isna()])
 # plt.title('Distribution of credit scores')
 # plt.grid()
 
-# sns.distplot(origination.mi_pct[~origination.mort_ins_pct.isna() & (origination.mort_ins_pct != 0)])
+# sns.distplot(origination.mort_ins_pct[~origination.mort_ins_pct.isna() & (origination.mort_ins_pct != 0)])
 # plt.title('Distribution of mortgage insurance percentage (%)')
 # plt.grid()
 
@@ -69,13 +74,22 @@ dum_1 = pd.get_dummies(origination.msa, prefix='msa', drop_first=True)
 # Performance wrangling
 monthly_performance_data_file = \
     "~/Desktop/historical_data1_2009/historical_data1_Q12009/historical_data1_time_Q12009.txt"
-performance_names = ["id_loan", "svcg_cycle", "current_upb", "delq_sts", "loan_age", "mths_remng", "repch_flag",
-                     "flag_mod", "cd_zero_bal", "dt_zero_bal", "current_int_rt", "non_int_brng_upb", "dt_lst_pi",
-                     "mi_recoveries", "net_sale_proceeds", "non_mi_recoveries", "expenses", "legal_costs",
+performance_names = ["id_loan", "monthly_reporting_period", "current_upb", "delq_sts", "loan_age", "mths_remng",
+                     "repch_flag", "flag_mod", "cd_zero_bal", "dt_zero_bal", "current_int_rt", "non_int_brng_upb",
+                     "dt_lst_pi", "mi_recoveries", "net_sale_proceeds", "non_mi_recoveries", "expenses", "legal_costs",
                      "maint_pres_costs", "taxes_ins_costs", "misc_costs", "actual_loss", "modcost", "stepmod_ind",
                      "dpm_ind", "eltv"]
-# st = datetime.now()
-# performance = pd.read_csv(monthly_performance_data_file, header=None, delimiter="|", names=performance_names)
-# print(f"time to read csvs: {datetime.now() - st}")
+st = datetime.now()
+performance = pd.read_csv(monthly_performance_data_file, header=None, delimiter="|", names=performance_names,
+                          na_values={'delq_sts': 'XX'},
+                          dtype={'delq_sts': 'category'})
+print(f"time to read csvs: {datetime.now() - st}")
+performance.monthly_reporting_period = pd.to_datetime(performance.monthly_reporting_period, format='%Y%m')
 
+tmp = performance[['id_loan', 'loan_age', 'delq_sts']].copy()
+
+test = pd.pivot_table(index=performance.id_loan,
+                      columns=performance.loan_age,
+                      values=performance.delq_sts,
+                      data=performance)
 
