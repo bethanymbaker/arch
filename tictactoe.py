035f9859048994599b56a1731d6c11d6ba94232a
Bethany Baker <bethany.baker@modaoperandi.com>
2019-09-09 11:53:49 -0700

    epic

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,21 @@
 import numpy as np
 
-def is_game_finished(board):
-    res = False
-    for row in board:
+
+def is_game_finished(bord):
+    to_test = [
+        bord[0],
+        bord[1],
+        bord[2],
+        [bord[0][0], bord[1][0], bord[2][0]],
+        [bord[0][1], bord[1][1], bord[2][1]],
+        [bord[0][2], bord[1][2], bord[2][2]],
+        [bord[0][0], bord[1][1], bord[2][2]],
+        [bord[0][2], bord[1][1], bord[2][0]]
+    ]
+    for row in to_test:
         if (row == [False, False, False]) | (row == [True, True, True]):
             return True
-    transposed = [[board[0][0], board[1][0], board[2][0]],
-                  [board[0][1], board[1][1], board[2][1]],
-                  [board[0][2], board[1][2], board[2][2]]]
-    for row in transposed:
-        if (row == [False, False, False]) | (row == [True, True, True]):
-            return True
-    for row in board:
+    for row in bord:
         for col in row:
             if col == '':
                 return False
@@ -19,22 +23,22 @@ def is_game_finished(board):
 
 
 all_boards = set()
-for game in range(100000):
+for game in range(50000):
     board = [['', '', ''] for i in range(3)]
-
-    flag = bool(np.random.randint(2))
-    kount = 1
     all_boards.add(str(board))
 
+    flag = bool(np.random.randint(2))
+
     while not is_game_finished(board):
-        switch = False
-        while not switch:
+        switch_turn = False
+        while not switch_turn:
             move = np.random.randint(9)
             row_num = move // 3
             col_num = move % 3
             if board[row_num][col_num] == '':
                 board[row_num][col_num] = flag
                 flag = not flag
-                switch = True
-            kount += 1
-            all_boards.add(str(board))
+                switch_turn = True
+                all_boards.add(str(board))
+
+print(f'num sets = {len(all_boards)}')
