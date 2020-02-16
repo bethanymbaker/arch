3ad85a5fecd200cf7cf048c27f4e172efcaec693
Bethany Baker <bakerbethanymarie@gmail.com>
2020-02-16 15:28:18 -0800

    code cleanup

--- deep_learning/siamese_net.py
+++ deep_learning/siamese_net.py
@@ -8,31 +8,20 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 200)
 pd.set_option('display.max_colwidth', 100)
 
-images_background_path = '/Users/bethanybaker/omniglot/python/images_background'
-images_evaluation_path = '/Users/bethanybaker/omniglot/python/images_evaluation'
 alphabet_dicts = []
-
-alphabet_list = [f for f in os.listdir(images_background_path) if not f.startswith('.')]
-for alphabet in alphabet_list:
-    character_list = [f for f in os.listdir(os.path.join(images_background_path, alphabet)) if not f.startswith('.')]
-    for character in character_list:
-        images = [f for f in os.listdir(os.path.join(images_background_path, alphabet, character)) if not f.startswith('.')]
-        for image in images:
-            alphabet_dicts.append({'alphabet': alphabet,
-                                   'character': character,
-                                   'image_path': os.path.join(images_background_path, alphabet, character, image),
-                                   'type': 'background'})
-
-alphabet_list = [f for f in os.listdir(images_evaluation_path) if not f.startswith('.')]
-for alphabet in alphabet_list:
-    character_list = [f for f in os.listdir(os.path.join(images_evaluation_path, alphabet)) if not f.startswith('.')]
-    for character in character_list:
-        images = [f for f in os.listdir(os.path.join(images_evaluation_path, alphabet, character)) if not f.startswith('.')]
-        for image in images:
-            alphabet_dicts.append({'alphabet': alphabet,
-                                   'character': character,
-                                   'image_path': os.path.join(images_evaluation_path, alphabet, character, image),
-                                   'type': 'evaluation'})
+image_types = ['images_background', 'images_evaluation']
+for image_type in image_types:
+    path = f'/Users/bethanybaker/omniglot/python/{image_type}'
+    alphabet_list = [f for f in os.listdir(path) if not f.startswith('.')]
+    for alphabet in alphabet_list:
+        character_list = [f for f in os.listdir(os.path.join(path, alphabet)) if not f.startswith('.')]
+        for character in character_list:
+            images = [f for f in os.listdir(os.path.join(path, alphabet, character)) if not f.startswith('.')]
+            for image in images:
+                alphabet_dicts.append({'alphabet': alphabet,
+                                       'character': character,
+                                       'image_path': os.path.join(path, alphabet, character, image),
+                                       'type': image_type.split('_')[1]})
 
 alphabet_df = pd.DataFrame(alphabet_dicts)
 
