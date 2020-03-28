9c615ec4c961b426ac2c10c9ab27e0e5dae2bfe5
Bethany Baker <bakerbethanymarie@gmail.com>
2020-03-28 16:12:58 -0700

    add python 3.8 interpreter ;)

--- tutorials/logging_tutorial.py
+++ tutorials/logging_tutorial.py
@@ -1,9 +1,15 @@
 import logging
+from datetime import datetime
 
 # logging.warning('Watch Out!')
 # logging.info('I told you so.')
 
+
 logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+logging.info(f"beginning query at {(st := datetime.now())}")
+logging.info(f"time to run query = {(datetime.now() - st).total_seconds():3f}")
+
 logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('And this, too')
