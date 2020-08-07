a57160961d6084e992294f3c64ecccce7ab2a119
Bethany Baker <bakerbethanymarie@gmail.com>
2020-08-07 11:40:21 -0700

    merged intervals

---
+++ leetcode/merge_intervals.py
@@ -0,0 +1,16 @@
+def merge(intervals):
+    sorted_intervals = sorted(intervals, key=lambda i: 10 * i[0] + i[1])
+    merged_intervals = [sorted_intervals.pop(0)]
+    left_interval = merged_intervals.pop()
+    for right_interval in sorted_intervals:
+        if right_interval[0] <= left_interval[1]:
+            merged_interval = [left_interval[0], right_interval[1]]
+            merged_intervals.append(merged_interval)
+        else:
+            merged_intervals.append(left_interval)
+            left_interval = right_interval
+    return merged_intervals
+
+
+ints = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge(ints))
