8b694545b5236f0215f2e1e1cbea918c41827bb7
Bethany Baker <bakerbethanymarie@gmail.com>
2020-08-07 11:47:31 -0700

    fix merge

--- leetcode/merge_intervals.py
+++ leetcode/merge_intervals.py
@@ -1,14 +1,15 @@
 def merge(intervals):
     sorted_intervals = sorted(intervals, key=lambda i: 10 * i[0] + i[1])
-    merged_intervals = [sorted_intervals.pop(0)]
-    left_interval = merged_intervals.pop()
+    merged_intervals = []
+    left_interval = sorted_intervals.pop(0)
     for right_interval in sorted_intervals:
         if right_interval[0] <= left_interval[1]:
             merged_interval = [left_interval[0], right_interval[1]]
-            merged_intervals.append(merged_interval)
+            left_interval = merged_interval
         else:
             merged_intervals.append(left_interval)
             left_interval = right_interval
+    merged_intervals.append(left_interval)
     return merged_intervals
 
 
